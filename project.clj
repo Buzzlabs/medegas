306f80337b8c429d1060d708e616487d7d644c77
Josué Santos <josue.santos.dev@gmail.com>
2023-01-11 11:49:26 -0300

    add tarsosDSP, clojure/math.numeric-tower

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject medegas "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME" 
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.github.st-h/TarsosDSP "2.4.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot medegas.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
