5ca1a97c84ca9a02bc1a5b0a02555111a5c53729
Josué Santos <josue.santos.dev@gmail.com>
2023-01-11 11:53:30 -0300

    bot config

--- src/medegas/core.clj
+++ src/medegas/core.clj
@@ -1,7 +1,22 @@
 (ns medegas.core
-  (:gen-class))
+  (:gen-class)
+  (:require [meinside.clogram :as cg]
+
+            [medegas.handlers :as handlers]))
+
+(def token (System/getenv "TOKEN_BOT"))
+
+(def bot (cg/new-bot token :verbose? true))
+(def interval 1)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (.addShutdownHook (Runtime/getRuntime)
+                    (Thread. #(do
+                                (println ">>> terminating application...")
+                                (cg/stop-polling-updates bot))))
+  (cg/poll-updates bot interval handlers/handle-bot))
+
+#_(-main)
