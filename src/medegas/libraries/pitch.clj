0c7d47b0cbeb67260393998b016ffb572a00d39f
Josué Santos <josue.santos.dev@gmail.com>
2023-01-16 16:41:31 -0300

    4096 -> 2048 in audioDispatcherFactory

--- src/medegas/libraries/pitch.clj
+++ src/medegas/libraries/pitch.clj
@@ -35,7 +35,7 @@
          0)))
 
 (defn detection-pitch [source]
-  (let [dispatcher (AudioDispatcherFactory/fromFile (io/file source) 4096 1024)
+  (let [dispatcher (AudioDispatcherFactory/fromFile (io/file source) 2048 1024)
         algorithm (PitchProcessor$PitchEstimationAlgorithm/YIN)
         pitch (atom [])
         pdh (reify PitchDetectionHandler
@@ -46,36 +46,42 @@
         pitch-processor (PitchProcessor. algorithm 48000 2048 pdh)]
     (.addAudioProcessor dispatcher pitch-processor)
     (.run dispatcher)
-    (println @pitch)
+    (println "pitch: " @pitch)
     (m @pitch)))
 
-(detection-pitch "resources/cheio.wav")
+
+(comment
+
+  (def pitch-cheio (detection-pitch "resources/cheio.wav"))
 ;; [3101.7964 3027.681 2972.9382 3012.6914 3031.5923 2998.6375 3075.689 2983.422 2961.255 3059.9988 3000.423 2988.0486 3005.1084 2966.1685 3010.116 3022.9795 3010.4536 3001.2773 2980.396 2981.8906 2988.5947 2992.0083 2995.5598 2981.1301 2978.1184 2977.2422 2974.5566 2978.9016 2975.8687 2974.8704 2968.6257]
 
-(detection-pitch "resources/usado.wav")
+  (detection-pitch "resources/usado.wav")
 ;; [3672.9734 3678.2732 3669.2021 3704.9304 3667.7002 3672.4224 3686.109 3689.4797 3740.7163 61.175144]
 
-(detection-pitch "resources/vazio.wav")
+  (m [3672.9734 3678.2732 3669.2021 3704.9304 3667.7002 3672.4224 3686.109 3689.4797 3740.7163 61.175144])
+  (histogram [3672.9734 3678.2732 3669.2021 3704.9304 3667.7002 3672.4224 3686.109 3689.4797 3740.7163 61.175144])
+  (filter-frequencies {36.0 [3672.9734 3678.2732 3669.2021 3667.7002 3672.4224 3686.109 3689.4797], 37.0 [3704.9304 3740.7163]})
+
+  (detection-pitch "resources/android3.m4a.wav")
 ;; [3507.6023]
-
-
+  )
 
 (defn fullness [medido]
   (try
     (let [d (- 1450 920)
           m (- 1450 (detection-pitch medido))]
-      (-> (/ m d)
+      (-> (quot m d)
           (* 100)
           (math/ceil)))
     (catch ArithmeticException _
       (str "divisão por zero"))))
 
-(fullness "resources/ExemploGasVazio.wav")
-(fullness "resources/ExemploGasVazio2.wav") 
+(comment (fullness "resources/ExemploGasVazio.wav")
+         (fullness "resources/ExemploGasVazio2.wav")
 
-(detection-pitch "resources/vazio.wav")
-(detection-pitch "resources/usado.wav")
-(detection-pitch "resources/cheio.wav")
+         (detection-pitch "resources/v2.wav")
+         (detection-pitch "resources/usado.wav")
+         (detection-pitch "resources/cheio.wav"))
 
 (defn medegas
   [file-path]
@@ -84,15 +90,16 @@
       -1
       value)))
 
-(medegas "resources/vazio.wav")
-(medegas "resources/usado.wav")
-(medegas "resources/cheio.wav")
+(comment (medegas "resources/ExemploGasVazio.wav")
+         (medegas "resources/ExemploGasVazio2.wav")
 
-(medegas "resources/ExemploGasVazio.wav")
-(medegas "resources/ExemploGasVazio2.wav")
+         (medegas "resources/iphone1.m4a.wav")
+         (medegas "resources/iphone2.m4a.wav")
+         (medegas "resources/iphone3.m4a.wav")
 
-(ogg-2-wav "resources/cima")
-(ogg-2-wav "resources/baixo")
+         (medegas "resources/android1.m4a.wav")
+         (medegas "resources/android2.m4a.wav")
+         (medegas "resources/android3.m4a.wav")
+         
+         (fullness "resources/android3.m4a.wav"))
 
-(medegas "resources/cima.mp4.wav")
-(medegas "resources/baixo.mp4.wav")
