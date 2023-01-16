7af8c186974161019ebbdda947ca6fae23a1789d
Josué Santos <josue.santos.dev@gmail.com>
2023-01-16 09:38:43 -0300

    bot: download files; replay message

--- src/medegas/handlers.clj
+++ src/medegas/handlers.clj
@@ -2,8 +2,16 @@
   (:require [clojure.java.io :as io]
 
             [meinside.clogram :as cg]
-            [meinside.clogram.helper :as cgh]))
+            [medegas.libraries.pitch :as lib.pitch]))
 
+(defn get-file
+  [bot file-id]
+  (let [{:keys [file-url file-path]} (:result (cg/get-file bot file-id))
+        file-output (str "resources/" file-path)]
+    (with-open [in (io/input-stream file-url)
+                out (io/output-stream file-output)]
+      (io/copy in out))
+    file-output))
 
 #_(use 'clojure.pprint)
 
@@ -20,12 +28,19 @@
         (println "*** failed to send chat action:" (:reason-pharse result))))
 
     (when file
-      (get-file bot file))
+      (let [file-path (get-file bot file)
+            _ (lib.pitch/ogg-2-wav file-path)
+            medegas (lib.pitch/medegas (str file-path ".wav"))
+            mede-text (str "seu gás esta aproximadamente em: " medegas)
+            result (cg/send-message bot chat-id mede-text
+                                    :reply-to-message-id reply-to)]
+        (when (not (:ok result))
+          (println "*** failed to send message:" (:reason-phrase result)))))
     (if (= text "/terminate") ;; DEV only
       (do
         (println ">>> received: /terminate")
         (cg/stop-polling-updates bot))
-      (let [echoed-text (str "echo: " text)
+      #_(let [echoed-text (str "echo: " text)
             result (cg/send-message bot chat-id echoed-text
                                     :reply-to-message-id reply-to)]
         (when (not (:ok result))
