0d509ff1770d6818a16c516f40d95e9b3865a60d
josue santos <josue.santos.dev@gmail.com>
2023-01-18 11:36:17 -0300

    :package: add clj-http, data.json

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.github.st-h/TarsosDSP "2.4.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [dev.meinside/clogram "0.15.0"]]
+                 [dev.meinside/clogram "0.15.0"]
+                 [clj-http "3.12.3"]
+                 [org.clojure/data.json "2.4.0"]]
   :main ^:skip-aot medegas.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
