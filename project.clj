b3c3a21bb051f6e4d5b72f8ef2ff4ab5e241c7d6
josue santos <josue.santos.dev@gmail.com>
2023-01-18 09:56:29 -0300

    :package: add datomic, tarsosDSP, math.numeric-tower

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
 (defproject medegas "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.pedestal/pedestal.service "0.5.11-beta-1"]
 
@@ -15,7 +17,11 @@
                  [ch.qos.logback/logback-classic "1.2.10" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.35"]
                  [org.slf4j/jcl-over-slf4j "1.7.35"]
-                 [org.slf4j/log4j-over-slf4j "1.7.35"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.35"]
+                 [com.datomic/datomic-pro "1.0.6527"]
+
+                 [com.github.st-h/TarsosDSP "2.4.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
