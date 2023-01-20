765055febf2fa3f12c0587e9d657d4f5760903f8
josue santos <josue.santos.dev@gmail.com>
2023-01-20 11:10:43 -0300

    hepl handler; wip

--- src/medegas/handlers.clj
+++ src/medegas/handlers.clj
@@ -1,17 +1,26 @@
 (ns medegas.handlers
-  (:require [clojure.java.io :as io]
-
-            [meinside.clogram :as cg]
-            [medegas.api :as api]))
+  (:require
+   [meinside.clogram :as cg]
+   [medegas.api :as api]))
 
 (defn get-file
   [bot file-id]
-  (let [{:keys [file-url file-path file-id file-unique-id]} (:result (cg/get-file bot file-id))
-        file-output (str "resources/" file-path)]
+  (let [{:keys [file-url file-path file-id file-unique-id]} (:result (cg/get-file bot file-id))]
     {:url file-url
      :output (str file-unique-id ".oga")}))
 
 #_(use 'clojure.pprint)
+
+(defn help
+  [bot chat-id]
+  (let [help-text (str "olá eu sou o medegas, um bot para te ajudar no dia a dia, descrobrir a quantidade aproximada do gas de cozinha.
+- pegue uma colher
+- coloque o celular proximo ao botijão de gás
+- bata com a colher não muito forte no botijão")
+        result (cg/send-message bot chat-id help-text)]
+    (println result)
+    (when-not (:ok result)
+      (println "*** failed to send message: \n" (:reason-pharse result)))))
 
 (defn handle-bot
   [bot update]
@@ -19,10 +28,8 @@
         msg-id (get-in update [:message :message-id])
         text (get-in update [:message :text])
         file (get-in update [:message :voice :file-id])]
-    #_(pprint file)
-    ;; 'typing...'
     (let [result (cg/send-chat-action bot chat-id :typing)]
-      (when (not (:ok result))
+      (when-not (:ok result)
         (println "*** failed to send chat action:" (:reason-pharse result))))
 
     (when file
@@ -30,11 +37,12 @@
             user-payload {:id (str chat-id) :type "telegram"}
             payload {:user user-payload :file file-payload}
             response (api/pitch-detect payload)
-            result (get-in response ["result"]) 
+            result (get-in response ["result"])
             mede-text (cond (string? response) response
                             (and (pos? result) (> 100 result)) (str "seu gás esta aproximadamente em: " result "%")
                             :else "ERROR: envie um audio novamente")
             result (cg/send-message bot chat-id mede-text
                                     :reply-to-message-id msg-id)]
-        (when (not (:ok result))
-          (println "*** failed to send message:" (:reason-phrase result)))))))
+        (when-not (:ok result)
+          (println "*** failed to send message:" (:reason-phrase result)))))
+    (cond (= text "/help") (help bot chat-id))))
