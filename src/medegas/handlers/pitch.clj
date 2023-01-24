6542cf3cbb9145cb74f9b643ea8a0b2a5bde6451
josue santos <josue.santos.dev@gmail.com>
2023-01-24 16:09:30 -0300

    [handlers] defn results;

--- src/medegas/handlers/pitch.clj
+++ src/medegas/handlers/pitch.clj
@@ -4,18 +4,25 @@
    [medegas.libraries.transactions.pitch :as tx.pitch]
    [medegas.libraries.database :as lib.db]))
 
+(defn results [{:keys [id]}]
+  (let [result (tx.pitch/get-result id lib.db/conn)]
+    (->> (group-by last result)
+         (map (fn [[k v]]
+                [k (first v)])))))
+
 (defn new-pitch
   [{:keys [json-params]}]
-  (let [{:keys [file user]} json-params
+  (let [{:keys [file user calibration]} json-params
         {:keys [url output]} file
         file-out (str "resources/" output)]
     (lib.detect/download-file url file-out)
     (lib.detect/oga-2-wav file-out)
-    (let [result (lib.detect/medegas (str file-out ".wav"))
+    (let [result (lib.detect/medegas (str file-out ".wav") (results user))
           payload (merge user file)]
-      (tx.pitch/tx-pitch (assoc payload :conn lib.db/conn
+      (tx.pitch/tx-pitch (assoc payload
                                 :result (str result)
-                                :tx-id (lib.db/create-tx-id 20)))
+                                :type (or calibration :calibration/default)
+                                :tx-id (lib.db/create-tx-id 20)) lib.db/conn)
       {:status 200
        :body {:result result}})))
 
