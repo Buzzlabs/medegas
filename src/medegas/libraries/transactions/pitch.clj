c4e0773351ce6b3f5eea69b97192887259b292f7
josue santos <josue.santos.dev@gmail.com>
2023-01-24 15:01:53 -0300

    [transaction] get-result;

--- src/medegas/libraries/transactions/pitch.clj
+++ src/medegas/libraries/transactions/pitch.clj
@@ -1,15 +1,15 @@
 (ns medegas.libraries.transactions.pitch
   (:require
-   [datomic.api :as d]))
+   [datomic.api :as d]
+   [medegas.libraries.database :as lib.db]))
 
 (defn tx-pitch
-  [{:keys [tx-id id result type calibration calibration-type conn]}]
-  (d/transact conn [[:db/add tx-id :pitch/id (java.util.UUID/randomUUID)]
-                    [:db/add tx-id :pitch/user id]
-                    [:db/add tx-id :pitch/type (keyword type)]
-                    [:db/add tx-id :pitch/result result]
-                    [:db/add tx-id :pitch/histogram calibration]
-                    [:db/add tx-id :pitch/calibration-type calibration-type]]))
+  [data conn]
+  (let [{:keys [tx-id id result type]} data]
+    (d/transact conn [[:db/add tx-id :pitch/id (java.util.UUID/randomUUID)]
+                      [:db/add tx-id :pitch/user id]
+                      [:db/add tx-id :pitch/type (keyword (str "calibration/" type))]
+                      [:db/add tx-id :pitch/result result]])))
 
 (defn get-pitch-by-user
   [user conn]
@@ -18,10 +18,12 @@
          :where
          [?e :pitch/user ?user]] (d/db conn) user))
 
-(defn get-histogram
+(defn get-result 
   [user conn]
-  (d/q '[:find ?e ?histogram
+  (d/q '[:find ?e ?result ?type
          :in $ ?user
          :where
          [?e :pitch/user ?user]
-         [?e :pitch/histogram ?histogram]] (d/db conn) user))
+         [?e :pitch/result ?result]
+         (or [?e :pitch/type ?type]
+             [?e :pitch/type ?type])] (d/db conn) user))
