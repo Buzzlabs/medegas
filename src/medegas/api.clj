c3217642442acc0d21c2c900d026c4f27045d06a
josue santos <josue.santos.dev@gmail.com>
2023-01-26 09:53:45 -0300

    (api) sound-type request

--- src/medegas/api.clj
+++ src/medegas/api.clj
@@ -16,11 +16,21 @@
                                :content-type :json})
              (resp->json))
          (catch Exception e 
+           (println e)
            "servico indisponivel no momento"))))
+
+(defn sound-type [payload]
+  (let [url (str base-url "pitches/types")]
+    (try (client/post url {:body (json/write-str payload)
+                           :content-type :json})
+         (catch Exception e
+           (println e)
+           "serviço indisponivel, tente mais tarde"))))
 
 (defn historic-pitch
   [id]
   (let [url (str base-url "pitches?user=" id)
         response (client/get url)]
     (json/read-str (:body response))))
+
 
