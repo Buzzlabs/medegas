a70f23605d1f490df374314a0e5e651569c7ccf1
josue santos <josue.santos.dev@gmail.com>
2023-01-26 09:49:00 -0300

    [transaction] tx-pitch-type

--- src/medegas/libraries/transactions/pitch.clj
+++ src/medegas/libraries/transactions/pitch.clj
@@ -5,11 +5,25 @@
 
 (defn tx-pitch
   [data conn]
-  (let [{:keys [tx-id id result type]} data]
+  (let [{:keys [tx-id id result type sound-id]} data]
     (d/transact conn [[:db/add tx-id :pitch/id (java.util.UUID/randomUUID)]
                       [:db/add tx-id :pitch/user id]
                       [:db/add tx-id :pitch/type (keyword (str "calibration/" type))]
-                      [:db/add tx-id :pitch/result result]])))
+                      [:db/add tx-id :pitch/result result]
+                      [:db/add tx-id :pitch/sound-id sound-id]])))
+
+(defn tx-pitch-type
+  [data conn]
+  (let [{:keys [tx-id type user sound]} data
+        [_ pitch-id] (first (d/q '[:find ?e ?id
+                                   :in $ ?user ?sound
+                                   :where
+                                   [?e :pitch/user ?user]
+                                   [?e :pitch/id ?id]
+                                   [?e :pitch/sound-id ?sound]] (d/db lib.db/conn) user sound))]
+    (d/transact conn [[:db/add tx-id :pitch/id pitch-id]
+                      [:db/add tx-id :pitch/sound-id sound]
+                      [:db/add tx-id :pitch/type (keyword (str "calibration/" type))]])))
 
 (defn get-pitch-by-user
   [user conn]
@@ -18,7 +32,7 @@
          :where
          [?e :pitch/user ?user]] (d/db conn) user))
 
-(defn get-result 
+(defn get-result
   [user conn]
   (d/q '[:find ?e ?result ?type
          :in $ ?user
