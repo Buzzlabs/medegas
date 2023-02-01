0e46cea8ae86cc505c7aaecc79efa1b917c17695
josue santos <josue.santos.dev@gmail.com>
2023-02-01 15:09:49 -0300

    add datomic, tarsosDsp and math.numeric

--- project.clj
+++ project.clj
@@ -1,13 +1,17 @@
 (defproject medegas "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.github.st-h/TarsosDSP "2.4.1"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [dev.meinside/clogram "0.15.0"]
-                 [clj-http "3.12.3"]
-                 [org.clojure/data.json "2.4.0"]]
+                 
+                 [com.datomic/datomic-pro "1.0.6527"]
+
+                 [com.github.st-h/TarsosDSP "2.4.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot medegas.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {:uberjar {:aot :all}})
