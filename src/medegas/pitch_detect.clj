2aa6d33d7e03e30b8d29ce4f366c57c46c82a96a
josue santos <josue.santos.dev@gmail.com>
2023-02-01 15:07:56 -0300

    pitch_detect from api to bot

---
+++ src/medegas/pitch_detect.clj
@@ -0,0 +1,84 @@
+(ns medegas.pitch-detect
+  (:require
+   [clojure.java.io :as io]
+   [clojure.java.shell :as shell]
+   [clojure.math.numeric-tower :as math])
+  (:import
+   (be.tarsos.dsp.pitch PitchDetectionHandler  PitchProcessor
+                        PitchProcessor$PitchEstimationAlgorithm)
+   (be.tarsos.dsp.io.jvm AudioDispatcherFactory)))
+
+(defn download-file
+  [file-url file-output]
+  (with-open [in (io/input-stream file-url)
+              out (io/output-stream file-output)]
+    (io/copy in out)))
+
+(defn oga-2-wav [file]
+  (let [out-file (str file ".wav")]
+    (shell/sh "ffmpeg" "-i" file  out-file)))
+
+(defn delete-file [files]
+  (doseq [file files]
+    (io/delete-file file)))
+
+(defn filter-frequencies [coll]
+  (filter #(> % 900) coll))
+
+(defn histogram [coll]
+  (reduce
+   (fn [acc x]
+     (let [p (quot x 100)
+           v (get acc p [])]
+       (update acc p (fn [_] (conj v x)))))
+   {} (filter-frequencies coll)))
+ 
+(defn m
+  [pitches]
+  (try (let [histogram (histogram pitches)
+             histogram-inter (map (fn [[key value]] {key (count value)}) histogram)
+             h-inter (if (empty? histogram-inter)
+                       '({1 1}) histogram-inter)
+             histogram-map (into {} h-inter)
+             histogram-key  (key (apply max-key val histogram-map))
+             histogram-freq (get histogram-map histogram-key)
+             histogram-total (apply + (get histogram histogram-key))]
+         (/ histogram-total histogram-freq))
+       (catch Exception e
+         (println e)
+         1)))
+
+(defn pitch-detect [source]
+  (let [dispatcher (AudioDispatcherFactory/fromFile (io/file source) 4096 1024)
+        algorithm (PitchProcessor$PitchEstimationAlgorithm/YIN)
+        pitch (atom [])
+        pdh (reify PitchDetectionHandler
+              (handlePitch [this result event]
+                (let [p (.getPitch result)]
+                  (when (pos? p)
+                    (swap! pitch conj p)))))
+        pitch-processor (PitchProcessor. algorithm 48000 2048 pdh)]
+    (.addAudioProcessor dispatcher pitch-processor)
+    (.run dispatcher)
+    (println "pitch: " @pitch)
+    (m @pitch)))
+
+(defn fullness [medido dt-cali]
+  (try
+    (let [value (pitch-detect medido)
+          dt-empty (:calibration/empty dt-cali)
+          dt-full (:calibration/full dt-cali)
+          d (- (or dt-full 1450) (or  dt-empty 920))
+          m (- (or dt-full 1450) value)]
+      (println dt-cali)
+      (-> (/ m d)
+          math/round))
+    (catch ArithmeticException _
+      (str "divisão por zero"))))
+
+(defn medegas
+  [file-path dt-cali]
+  (let [value (fullness file-path dt-cali)]
+    (if (string? value)
+      -1
+      value)))
